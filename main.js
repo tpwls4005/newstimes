1618a2d469b70018799903dc824638a2bc9581e9
sejin min <tpwls4005@naver.com>
2024-02-22 01:22:15 +0900

    commit2

--- main.js
+++ main.js
@@ -1,12 +1,20 @@
 const API_KEY ='4778314702624d38b8918d9c8dbb8b06';
-let news = []
+const url1= `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`;
+const url2="https://rococo-croissant-3ff41d.netlify.app/";
+
+let news = [];
 const getLatesNews = async()=> {
-  const url = new URL (`https://rococo-croissant-3ff41d.netlify.app/`);
-  // url
-  const response = await fetch(url); //await 비동기
+  const requestUrl = new URL(url2);
+  console.log("dddddd", requestUrl);
+
+  const response = await fetch(url1); //await 비동기
   const data = await response.json()
-  news = data.articles
+  news = data.articles;
+  console.log("rrr", response);
+  console.log("dddddd", data);
   console.log("dddddd", news);
 }
 
 getLatesNews();
+
+/* https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY} */
