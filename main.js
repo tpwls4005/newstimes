da98d057b6023eb793dc4cec6b94754c0997f5a1
sejin min <tpwls4005@naver.com>
2024-02-22 14:43:42 +0900

    api 배포

--- main.js
+++ main.js
@@ -1,20 +1,28 @@
 const API_KEY ='4778314702624d38b8918d9c8dbb8b06';
 const url1= `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`;
-const url2="https://rococo-croissant-3ff41d.netlify.app/";
+const url2="https://rococo-croissant-3ff41d.netlify.app//top-headlines?country=kr&apiKey=${API_KEY}";
 
-let news = [];
+let newsList = [];
 const getLatesNews = async()=> {
   const requestUrl = new URL(url2);
   console.log("dddddd", requestUrl);
 
   const response = await fetch(url1); //await 비동기
   const data = await response.json()
-  news = data.articles;
+  newsList = data.articles;
   console.log("rrr", response);
   console.log("dddddd", data);
-  console.log("dddddd", news);
+  console.log("dddddd", newsList);
 }
 
+const render = () => {
+  const newsHTML = ``;
+
+  newsHTML=newsList.map(new=>``)
+
+  document.getElementById("news-board").innerHTML = newsHTML;
+}
 getLatesNews();
+
 
 /* https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY} */
