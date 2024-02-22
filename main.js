cb0b91653c610f2f86be9533a9118314ad7c73dd
sejin min <tpwls4005@naver.com>
2024-02-23 02:41:48 +0900

    8탄

--- main.js
+++ main.js
@@ -1,55 +1,41 @@
-const API_KEY ='4778314702624d38b8918d9c8dbb8b06';
-const url1= `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`;
-const url2= `http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines`;
-const url3=`https://rococo-croissant-3ff41d.netlify.app/top-headlines`;
+const API_KEY = '4778314702624d38b8918d9c8dbb8b06';
+const url1 = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`;
+const url2 = `http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines`;
+const url3 = `https://rococo-croissant-3ff41d.netlify.app/top-headlines`;
 
 let newsList = [];
-const getLatesNews = async()=> {
+const getLatesNews = async () => {
   const url = new URL(url3);
   // console.log("uuu:, url")
 
   const response = await fetch(url);
   const data = await response.json();
   newsList = data.articles;
-  render();
   // console.log("aaa", response);
   // console.log("bbb", data);
   console.log("ccc", newsList);
 };
 
 const render = () => {
-  let resultHTML = articles
-  .map((news) => {
-    return `<div class="news row">
-        <div class="col-lg-4">
-            <img class="news-img"
-                src="${
-        news.urlToImage ||
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqEWgS0uxxEYJ0PsOb2OgwyWvC0Gjp8NUdPw&usqp=CAU"
-    }" />
+  const newsHTML = ``;
+  newsHTML = newsList.map(news => `<div class="row news">
+      <div class="col-lg-4">
+        <img class="news-img-size"
+             src=""${news.urlToImage} />
+      </div>
+      <div class="col-lg-8">
+        <h2>${news.title}</h2>
+        <p>
+          ${news.description}
+        </p>
+        <div>
+          ${news.source.name} * ${news.publishedAt}
         </div>
-        <div class="col-lg-8">
-            <a class="title" target="_blank" href="${news.url}">${
-        news.title
-    }</a>
-            <p>${
-        news.description == null || news.description == ""
-            ? "내용없음"
-            : news.description.length > 200
-                ? news.description.substring(0, 200) + "..."
-                : news.description
-    }</p>
-            <div>${news.source.name || "no source"}  ${moment(
-        news.publishedAt
-    ).fromNow()}</div>
-        </div>
-    </div>`;
-  })
-  .join("");
-
-  document.getElementById("news-board").innerHTML = resultHTML;
+      </div>
+    </div>`)
+  document.getElementById("news-board").innerHTML = newsHTML;
 };
-/* Set the width of the side navigation to 250px */
+
 function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
 }
