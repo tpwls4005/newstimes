8fe61e8a98f84f164608912c071cbd34d871b8c4
sejin min <tpwls4005@naver.com>
2024-02-22 16:55:45 +0900

    api 배포

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const API_KEY ='4778314702624d38b8918d9c8dbb8b06';
 const url1= `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`;
 const url2= `http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines`;
-const url3=`https://rococo-croissant-3ff41d.netlify.app/`;
+const url3=`https://rococo-croissant-3ff41d.netlify.app/top-headlines`;
 
 let newsList = [];
 const getLatesNews = async()=> {
