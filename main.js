be8d73d5aebc7dc8a25b23c6a418d3e9a3885b58
sejin min <tpwls4005@naver.com>
2024-02-23 23:43:08 +0900

    검색결과가 없습니다.

--- main.js
+++ main.js
@@ -9,66 +9,126 @@ menus.forEach(menu =>
     menu.addEventListener("click", (event) => getNewsByCategory(event))
 );
 
- const getNews=async ()=>{
-  const response = await fetch(url);
+const getNews = async () => {
+  try {
+    const response = await fetch(url);
+
+    const data = await response.json();
+    if (response.status === 200) {
+      if (data.articles.length === 0) {
+        throw new Error("검색 결과가 없습니다.")
+      }
+      newsList = data.articles;
+      render();
+    } else {
+      throw new Error(data.meaasage);
+    }
+  } catch (error) {
+    errorRender(error.message)
+  }
+};
+
+const getWeNews = async ()=>{
+  const response = await fetch(url)
   const data = await response.json();
   newsList = data.articles;
   render();
 }
+
+
+menus.forEach((menu) =>
+    menu.addEventListener("click", (event) => getNewsByCategory(event))
+);
+
+const searches = document.querySelectorAll(".search-button");
+
+searches.forEach((search) =>
+    search.addEventListener("click", (e) => getNewsByKeyword(e))
+);
+
+
 let url = new URL(`https://rococo-croissant-3ff41d.netlify.app/top-headlines`)
 const getLatesNews = async () => {
-url = new URL(url3);
+  url = new URL(url3);
   getNews();
 };
-const getNewsByCategory = async(event) => {
+const getNewsByCategory = async (event) => {
   const category = event.target.textContent.toLowerCase();
-  console.log("category");
   url = new URL(
       `https://rococo-croissant-3ff41d.netlify.app/top-headlines?category=${category}`
   );
   getNews();
 };
 
-const getNewsByKeyword=async ()=>{
-  const keyword= document.getElementById("search-input").value;
-  console.log("keyword", keyword);
+const getNewsByKeyword = async (event) => {
+  const keyword = document.getElementById("search-input").value;
   url = new URL(
       `https://rococo-croissant-3ff41d.netlify.app/top-headlines?q=${keyword}`
   );
   getNews();
+}
+
+const validateImageUrl = (imageUrl) => {
+  // image객체 생성
+  const image = new Image();
+  image.src = imageUrl;
+  return image.complete || (image.width + image.height) > 0;
 };
 
 const render = () => {
-  const newsHTML = newsList.map(news =>
-      `<div class="row news">
-      <div class="col-lg-4">
-        <img class="news-img-size" src="${news.urlToImage}" />
-      </div>
-      <div class="col-lg-8">
-        <h2>${news.title}</h2>
-        <img class="news-img"
-          src="${
-          news.media ||
-          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqEWgS0uxxEYJ0PsOb2OgwyWvC0Gjp8NUdPw&usqp=CAU"
-      }" />
-        <p>${news.description}</p>
-        <p>${
-          news.summary == null || news.summary == ""
-              ? "내용없음"
-              : news.summary.length > 200
-                  ? news.summary.substring(0, 200) + "..."
-                  : news.summary
-      }</p>
-        <div>${news.source.name} * ${news.publishedAt}</div>
-      </div>
-    </div>`
-  ).join('');
+  const validateImageUrl = (imageUrl) => {
+    const image = new Image();
+    image.src = imageUrl;
+    // console.log(image.src);
+    return image.complete || (image.width + image.height) > 0;
+  };
+
+  const newsHTML = newsList.map(
+      (news) => {
+        let imageUrl = news.urlToImage ? news.urlToImage : '/images/noImg.jpg';
+        const validateImage = validateImageUrl(imageUrl);
+        if (!validateImage) {
+          imageUrl = '/images/noImg.jpg';
+        }
+        return`<div class="row news">
+    <div class="col-lg-4">
+      <img class="news-img-size" src= ${imageUrl}
+      />
+    </div>    
+    <div class="col-lg-8">
+      <h2>${news.title}</h2>
+      <p>
+        ${
+            news.description == null || news.description == ""
+                ? "내용없음"
+                : news.description.length > 200
+                    ? news.description.substring(0, 200) + "..."
+                    : news.description
+        }
+     </p>
+           
+      <div>        
+        ${news.source.name == null || news.source.name == ""
+            ? "no source"
+            : news.source.name
+        } * ${news.publishedAt}
+      </div>      
+    </div>
+  </div>`
+      }).join('');
 
   document.getElementById("news-board").innerHTML = newsHTML;
 };
 // 1. 버튼들에게 클릭 이벤트 주기
 // 2. 카테고리 별 뉴스 가져오기
 // 3. 그 뉴스 보여주기
+
+const errorRender = (errorMessage) => {
+  const errorHTML = `<div class="alert alert-danger" role="alert">
+${errorMessage}</div>`
+
+  document.getElementById("news-board").innerHTML = errorHTML;
+}
 
 function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
@@ -87,7 +147,5 @@ const openSearchBox = () => {
     inputArea.style.display = "inline";
   }
 };
-
-
 
 getLatesNews();
