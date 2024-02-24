2c91ac8071037c570b8a75e216cbaa247ef10fe4
sejin min <tpwls4005@naver.com>
2024-02-25 01:55:39 +0900

    Page navigation

--- main.js
+++ main.js
@@ -9,32 +9,43 @@ menus.forEach(menu =>
     menu.addEventListener("click", (event) => getNewsByCategory(event))
 );
 
+let totalResults = 0;
+let page = 1;
+const pageSize = 10;
+const groupSize = 5;
+
 const getNews = async () => {
   try {
+    url.searchParams.set("page", page);
+    url.searchParams.set("pageSize", pageSize);
+
     const response = await fetch(url);
 
     const data = await response.json();
     if (response.status === 200) {
       if (data.articles.length === 0) {
-        throw new Error("검색 결과가 없습니다.")
+        throw new Error("No result for this search..");
+      } else {
+        newsList = data.articles;
+        totalResults = data.totalResults;
+        render();
+        paginationRender();
       }
-      newsList = data.articles;
-      render();
     } else {
-      throw new Error(data.meaasage);
+      throw new Error(data.message);
     }
   } catch (error) {
-    errorRender(error.message)
+    errorRender(error.message);
+    console.log("error : ", error.message);
   }
 };
 
-const getWeNews = async ()=>{
+const getWeNews = async () => {
   const response = await fetch(url)
   const data = await response.json();
   newsList = data.articles;
   render();
 }
-
 
 menus.forEach((menu) =>
     menu.addEventListener("click", (event) => getNewsByCategory(event))
@@ -45,7 +56,6 @@ const searches = document.querySelectorAll(".search-button");
 searches.forEach((search) =>
     search.addEventListener("click", (e) => getNewsByKeyword(e))
 );
-
 
 let url = new URL(`https://rococo-croissant-3ff41d.netlify.app/top-headlines`)
 const getLatesNews = async () => {
@@ -90,7 +100,7 @@ const render = () => {
         if (!validateImage) {
           imageUrl = '/images/noImg.jpg';
         }
-        return`<div class="row news">
+        return `<div class="row news">
     <div class="col-lg-4">
       <img class="news-img-size" src= ${imageUrl}
       />
@@ -146,6 +156,32 @@ const openSearchBox = () => {
   } else {
     inputArea.style.display = "inline";
   }
+};
+const paginationRender = () => {
+  const totalPages = Math.ceil(totalResults / pageSize);
+  const pageGroup = Math.ceil(page / groupSize);
+  let lastPage = pageGroup * groupSize;
+  if (lastPage > totalPages) {
+    lastPage = totalPages;
+  }
+  const firstPage =
+      lastPage - (groupSize - 1) <= 0 ? 1 : lastPage - (groupSize - 1);
+
+  let paginationHTML = "";
+
+  for (let i = firstPage; i <= lastPage; i++) {
+    paginationHTML += `<li class="page-item" ${
+        i === page ? "active" : ""
+    } onclick = "moveToPage(${i})"><a class="page-link">${i}</a></li>`;
+  }
+
+  document.querySelector(".pagination").innerHTML = paginationHTML;
+};
+
+let moveToPage = (pageNum) => {
+  console.log("MMMM", pageNum);
+  getNews();
+  page = pageNum;
 };
 
 getLatesNews();
